7f60d3fb6a3bce0586ee360c9245c4a8ad4b5b21
BNTAnnn <157035235+BNTAnnn@users.noreply.github.com>
2025-11-29 23:16:45 +0700

    Update VaccineTemplatesDao.java
    Removed unused methods from VaccineTemplatesDao interface.

--- EHR/src/main/java/com/mycompany/ehr/bnta/dao/VaccineTemplatesDao.java
+++ EHR/src/main/java/com/mycompany/ehr/bnta/dao/VaccineTemplatesDao.java
@@ -6,17 +6,7 @@ import java.util.ArrayList;
 
 public interface VaccineTemplatesDao {
 
-    int insert(VaccineTemplates t);
-
     VaccineTemplates selectById(VaccineTemplates t);
 
-    int update(VaccineTemplates t);
-
-    int delete(VaccineTemplates t);
-
     ArrayList<VaccineTemplates> selectAll(); 
-
-    ArrayList<VaccineTemplates> selectByCondition(String condition);
-
-    boolean exists(String condition);
 }
