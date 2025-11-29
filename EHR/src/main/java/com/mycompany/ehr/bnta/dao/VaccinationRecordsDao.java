039d4dcc491d634e5c68d5106798aeb54061afce
BNTAnnn <157035235+BNTAnnn@users.noreply.github.com>
2025-11-29 23:15:45 +0700

    Update VaccinationRecordsDao.java

--- EHR/src/main/java/com/mycompany/ehr/bnta/dao/VaccinationRecordsDao.java
+++ EHR/src/main/java/com/mycompany/ehr/bnta/dao/VaccinationRecordsDao.java
@@ -6,17 +6,9 @@ import java.util.ArrayList;
 
 public interface VaccinationRecordsDao {
 
-    int insert(VaccinationRecords t);
-
     VaccinationRecords selectById(VaccinationRecords t);
-
-    int update(VaccinationRecords t);
-
-    int delete(VaccinationRecords t);
 
     ArrayList<VaccinationRecords> selectAll();
 
-    ArrayList<VaccinationRecords> selectByCondition(String condition);
-
-    boolean exists(String condition);
+    ArrayList<VaccinationRecords> selectByMemberId(int memberId);
 }
